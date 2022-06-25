98220f2b67bc1e404461c1f6acea844700b94e13
Febreeze75 <febreezebooz@gmail.com>
2022-06-24 21:31:36 -0400

    Add project files.

---
+++ RecommendedCodeWars/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RecommendedCodeWars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = "";
+
+            foreach (var ch in input)
+            {
+                if (ch != ' ')
+                {
+                    output += ch;
+                }
+            }
+
+            return output;
+        }
+    }
+}
